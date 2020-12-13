af99a85bf2be2bceef95a7da15a83f433fc1000e
patryk baryla <patryk.baryla@spoko.pl>
2020-12-13 18:02:24 +0100

    Activity

--- activity.py
+++ activity.py
@@ -1,29 +1,21 @@
 #!/usr/bin/env python3
+import json
 import subprocess
 import time
-import os
 from operator import itemgetter
 
-# -- set update/round time (seconds)
-period = 5 
-# -- set sorting order. up = most used first, use either "up" or "down"
-order = "up"
 
-# don change anything below
-home = os.environ["HOME"]
-logdir = home+"/.usagelogs"
+def get_current_time(format_time=None):
+    return time.strftime("%Y_%m_%d_%H_%M_%S") if format_time == "file" \
+        else time.strftime("%Y-%m-%d %H:%M:%S")
 
-def currtime(tformat=None):
-    return time.strftime("%Y_%m_%d_%H_%M_%S") if tformat == "file"\
-           else time.strftime("%Y-%m-%d %H:%M:%S")
 
-try:
-    os.mkdir(logdir)
-except FileExistsError:
-    pass
+def time_format(s):
+    # convert time format from seconds to h:m:s
+    m, s = divmod(s, 60)
+    h, m = divmod(m, 60)
+    return "%d:%02d:%02d" % (h, m, s)
 
-# path to your logfile
-log = logdir+"/"+currtime("file")+".txt"; startt = currtime()
 
 def get(command):
     try:
@@ -31,73 +23,91 @@ def get(command):
     except subprocess.CalledProcessError:
         pass
 
-def time_format(s):
-    # convert time format from seconds to h:m:s
-    m, s = divmod(s, 60); h, m = divmod(m, 60)
-    return "%d:%02d:%02d" % (h, m, s)
 
-def summarize():
-    with open(log, "wt" ) as report:
-        totaltime = sum([it[2] for it in winlist]) # total time
-        report.write("")
-        alldata = []      
-        for app in applist:
-            appdata = []; windata = []
-            apptime = sum([it[2] for it in winlist if it[0] == app])
-            appperc = round(100*apptime/totaltime)            
-            for d in [app, apptime, appperc]:
+class ActivityLogger:
+    def __init__(self):
+        self.period = 5
+        self.order = "up"
+        self.start_time = get_current_time()
+        self.t = 0
+        self.list_of_active_apps = []
+        self.list_of_activity_in_apps = []
+
+    def run(self):
+        while True:
+            frpid = get(["xdotool", "getactivewindow", "getwindowpid"])
+            frname = get(["xdotool", "getactivewindow", "getwindowname"])
+            app = get([
+                "ps", "-p", frpid, "-o", "comm="
+            ]) if frpid is not None else "Unknown"
+            # fix a few names
+            if "gnome-terminal" in app:
+                app = "gnome-terminal"
+            elif app == "soffice.bin":
+                app = "libreoffice"
+            # add app to list
+            if app not in self.list_of_active_apps:
+                self.list_of_active_apps.append(app)
+            checklist = [item[1] for item in self.list_of_activity_in_apps]
+            if frname not in checklist:
+                self.list_of_activity_in_apps.append([app, frname, 1 * self.period])
+            else:
+                self.list_of_activity_in_apps[checklist.index(frname)][
+                    2] = self.list_of_activity_in_apps[checklist.index(frname)][2] + 1 * self.period
+            if self.t == 60 / self.period:
+                self.summarize()
+                self.t = 0
+            else:
+                self.t += 1
+            time.sleep(self.period)
+
+    def summarize(self):
+        total_time = sum([it[2] for it in self.list_of_activity_in_apps])
+        all_data = []
+        for app in self.list_of_active_apps:
+            appdata = []
+            window_data = []
+            app_time = sum([it[2] for it in self.list_of_activity_in_apps if it[0] == app])
+            app_percents = round(100 * app_time / total_time)
+            for d in [app, app_time, app_percents]:
                 appdata.append(d)
-            wins = [r for r in winlist if r[0] == app]            
+            wins = [r for r in self.list_of_activity_in_apps if r[0] == app]
             for w in wins:
-                wperc = str(round(100*w[2]/totaltime))
-                windata.append([w[1], w[2], wperc])                
-            windata = sorted(windata, key=itemgetter(1))
-            windata = windata[::-1] if order == "up" else windata
-            appdata.append(windata); alldata.append(appdata)            
-        alldata = sorted(alldata, key = itemgetter(1))
-        alldata = alldata[::-1] if order == "up" else alldata        
-        for item in alldata:
-            app = item[0]; apptime = item[1]; appperc = item[2]
-            report.write(
-                ("-"*60)+"\n"+app+"\n"+time_format(apptime)\
-                +" ("+str(appperc)+"%)\n"+("-"*60)+"\n"
-                )            
+                window_percents = str(round(100 * w[2] / total_time))
+                window_data.append([w[1], w[2], window_percents])
+            window_data = sorted(window_data, key=itemgetter(1))
+            window_data = window_data[::-1] if self.order == "up" else window_data
+            appdata.append(window_data)
+            all_data.append(appdata)
+        all_data = sorted(all_data, key=itemgetter(1))
+        all_data = all_data[::-1] if self.order == "up" else all_data
+
+        list_to_save = []
+
+        for item in all_data:
+            dict_app = {}
+            app = item[0]
+            app_time = item[1]
+            app_percents = item[2]
+            dict_app["app"] = str(app)
+            dict_app["time"] = time_format(app_time)
+            dict_app["percentage"] = str(app_percents)
+            windows_list = []
             for w in item[3]:
-                wname = w[0]; time = w[1]; perc = w[2]
-                report.write(
-                    "   "+time_format(time)+" ("+perc+"%)"\
-                    +(6-len(perc))*" "+wname+"\n"
-                    )
-        report.write(
-            "\n"+"="*60+"\nstarted: "+startt+"\t"+"updated: "\
-            +currtime()+"\n"+"="*60
-            )
+                window_dict = {}
+                window_name = w[0]
+                window_time = w[1]
+                window_percents = w[2]
+                window_dict["window_name"] = str(window_name)
+                window_dict["window_time"] = time_format(window_time)
+                window_dict["window_percentage"] = window_percents
+                windows_list.append(window_dict)
+            dict_app["windows"] = windows_list
+            list_to_save.append(dict_app)
+        with open('log_activity.json', 'w') as out:
+            json.dump(list_to_save, out, sort_keys=True, indent=4, separators=(',', ': '))
 
-t = 0; applist = []; winlist = []
 
-while True:
-    time.sleep(period)
-    frpid = get(["xdotool", "getactivewindow", "getwindowpid"])
-    frname = get(["xdotool", "getactivewindow", "getwindowname"])
-    app = get([
-        "ps", "-p", frpid, "-o", "comm="
-        ]) if frpid != None else "Unknown"
-    # fix a few names
-    if "gnome-terminal" in app:
-        app = "gnome-terminal"
-    elif app == "soffice.bin":
-        app = "libreoffice"
-    # add app to list
-    if not app in applist:
-        applist.append(app)
-    checklist = [item[1] for item in winlist]
-    if not frname in checklist:
-        winlist.append([app, frname, 1*period])
-    else:
-        winlist[checklist.index(frname)][
-            2] = winlist[checklist.index(frname)][2]+1*period
-    if t == 60/period:
-        summarize()
-        t = 0
-    else:
-        t += 1
+if __name__ == '__main__':
+    logger = ActivityLogger()
+    logger.run()
